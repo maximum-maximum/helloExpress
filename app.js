e7d9f787bdbcf1272514af9872a636723570b801
Koki Makishima <mr_ver74@yahoo.co.jp>
2021-06-04 22:59:19 +0900

    パーシャルの書き換え

--- app.js
+++ app.js
@@ -43,12 +43,12 @@ function getFromClient(request, response) {
 }
 
 // 追加するデータ用変数
-var data = {
-    'Taro': '09-999-999',
-    'Hanako': '080-888-888',
-    'Sachiko': '070-777-777',
-    'Ichiro': '060-666-666'
-  };
+var data2 = {
+  'Taro': ['taro@yamada', '09-999-999', 'Tokyo'],
+  'Hanako': ['hanako@flower', '080-888-888', 'Yokohama'],
+  'Sachiko': ['sachi@happy', '070-777-777', 'Nagoya'],
+  'Ichiro': ['ichi@baseball', '060-666-666', 'USA'],
+}
   
   // indexのアクセス処理
   function response_index(request, response) {
@@ -56,7 +56,7 @@ var data = {
     var content = ejs.render(index_page, {
       title: "Index",
       content: msg,
-      data: data,
+      data: data2,
       filename: 'data_item' //☆追記
     });
     response.writeHead(200, { 'Content-Type': 'text/html' });
@@ -67,38 +67,13 @@ var data = {
 // ★otherのアクセス処理
 function response_other(request, response) {
   var msg = "これはOtherページです。"
-
-  // POSTアクセス時の処理
-  if (request.method == 'POST') {
-    var body = '';
-
-    // データ受信のイベント処理
-    request.on('data', (data) => {
-      body += data;
-    });
-
-    // データ受信終了のイベント処理
-    request.on('end', () => {
-      var post_data = qs.parse(body); // ★データのパース
-      msg += 'あなたは、「' + post_data.msg + '」と書きました。';
-      var content = ejs.render(other_page, {
-        title: "Other",
-        content: msg,
-      });
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.write(content);
-      response.end();
-    });
-
-    // GETアクセス時の処理
-  } else {
-    var msg = "ページがありません。"
-    var content = ejs.render(other_page, {
-      title: "Other",
-      content: msg,
-    });
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(content);
-    response.end();
-  }
+  var content = ejs.render(other_page, {
+    title: "Other",
+    content: msg,
+    data: data2,
+    filename: 'data_item'
+  });
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(content);
+  response.end();
 }
