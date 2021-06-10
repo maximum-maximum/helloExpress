424ce082ff570c9c9923d9a82b5bd2c904fec2f1
Koki Makishima <mr_ver74@yahoo.co.jp>
2021-06-10 14:58:33 +0900

    userの追加

--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/index');
+const { Op } = require("sequelize");
 
 /* GET users listing. */
-router.get('/',(req, res, next)=> {
+router.get('/',(req, res, next) =>{
   db.User.findAll().then(usrs => {
     var data = {
       title: 'Users/Index',
@@ -11,6 +12,26 @@ router.get('/',(req, res, next)=> {
     }
     res.render('users/index', data);
   });
+});
+
+router.get('/add',(req, res, next)=> {
+  var data = {
+    title: 'Users/Add'
+  }
+  res.render('users/add', data);
+});
+
+router.post('/add',(req, res, next)=> {
+  db.sequelize.sync()
+    .then(() => db.User.create({
+      name: req.body.name,
+      pass: req.body.pass,
+      mail: req.body.mail,
+      age: req.body.age
+    }))
+    .then(usr => {
+      res.redirect('/users');
+    });
 });
 
 module.exports = router;
