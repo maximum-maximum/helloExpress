dbdda7e7e634f983493ed7d52f6cf9f2e4f07ccc
Koki Makishima <mr_ver74@yahoo.co.jp>
2021-06-12 03:45:10 +0900

    一通りfix

--- ex-gen-app/app.js
+++ ex-gen-app/app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hello = require('./routes/hello');
+var boardsRouter = require('./routes/boards');
 
 var app = express();
 
@@ -32,6 +33,7 @@ app.use(session(session_opt));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', hello);
+app.use('/boards', boardsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
